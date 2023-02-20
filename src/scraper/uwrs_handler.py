9dc2f82716f5327d76324cead72b8f45074ef175
Lucas Salas <salas.lucas@gmail.com>
2023-02-20 13:36:08 -0700

    created demographics_count_not_found

--- src/scraper/uwrs_handler.py
+++ src/scraper/uwrs_handler.py
@@ -144,3 +144,21 @@ def save_no_demographics(summary_df, term_id):
 def load_demographics():
     # TODO create an SQL database for student demographic info
     return pd.read_excel('../../resources/demographics.xlsx')
+
+
+def demographics_count_not_found(demographics_info, uwrs_df):
+    uwrs_names = uwrs_df['name'].str.lower().tolist()
+    tmp_demog_names = demographics_info['First Name'] + ' ' + demographics_info['Last Name']
+    demog_names = tmp_demog_names.str.lower().tolist()
+
+    not_found_count = 0
+    found_count = 0
+
+    for name in uwrs_names:
+        if name not in demog_names:
+            not_found_count += 1
+        else:
+            found_count += 1
+
+    print(f"Found: {found_count}")
+    print(f"Not Found: {not_found_count}")
