c536aac943a1a9e2f9bbb9466554186af367ddf1
Lucas Salas <salas.lucas@gmail.com>
2023-03-02 19:23:03 -0700

    Extract UWRS class to module
    The UwrsHandler class was unnecessary and better suited as a module.

--- src/scraper/uwrs_handler.py
+++ src/scraper/uwrs_handler.py
@@ -15,95 +15,91 @@ class UwrsListPair:
     post_list = []
 
 
-class UwrsHandler:
+def get_uwrs_quizzes(enrollment_term, pre_post, canwrap, course_designation):
+    """
+    Function to consolidate scraper functionality, specific to uwrs quizzes
+    :param int enrollment_term:
+    :param str pre_post:
+    """
+    # pre_post validation
+    if pre_post not in ["pre", "post"]:
+        raise ValueError
+    search_terms = SearchTerms("Resilience Questionnaire (Pre-Assessment)",
+                               "Resilience Questionnaire (Post-Assessment)")._asdict()
+    # Pull all account courses
+    master_course_list = canwrap.get_account_courses(enrollment_term)
+    # Filter out non-HLAC courses
+    filtered_courses = quiz_scraper.SearchHandler.filter_courses(master_course_list, course_designation)
+    # Get all UWRS quizzes for course in course list
+    search_results = quiz_scraper.SearchHandler.search_quizzes(filtered_courses, search_terms[pre_post])
+    rph = quiz_scraper.ReportHandler(search_results)
+    updated_quiz_list = rph.fetch_updated_reports(canwrap.canvas)
 
-    def __init__(self, canwrap: quiz_scraper.CanvasWrapper):
-        self.canwrap = canwrap
-        self.course_designation = "HLAC"
+    return quiz_scraper.build_quiz_wrappers(updated_quiz_list)
 
-    def get_uwrs_quizzes(self, enrollment_term, pre_post):
-        """
-        Function to consolidate scraper functionality, specific to uwrs quizzes
-        :param int enrollment_term:
-        :param str pre_post:
-        """
-        # pre_post validation
-        if pre_post not in ["pre", "post"]:
-            raise ValueError
-        search_terms = SearchTerms("Resilience Questionnaire (Pre-Assessment)",
-                                   "Resilience Questionnaire (Post-Assessment)")._asdict()
-        # Pull all account courses
-        master_course_list = self.canwrap.get_account_courses(enrollment_term)
-        # Filter out non-HLAC courses
-        filtered_courses = quiz_scraper.SearchHandler.filter_courses(master_course_list, self.course_designation)
-        # Get all UWRS quizzes for course in course list
-        search_results = quiz_scraper.SearchHandler.search_quizzes(filtered_courses, search_terms[pre_post])
-        rph = quiz_scraper.ReportHandler(search_results)
-        updated_quiz_list = rph.fetch_updated_reports(self.canwrap.canvas)
 
-        return quiz_scraper.build_quiz_wrappers(updated_quiz_list)
+def build_df_list(wrapped_list):
+    """
+    Build dataframe list from list of report download urls
+    :param wrapped_list: list[quiz_scraper.QuizWrapper]
+    :return: list[pandas.Dataframe]
+    """
+    df_list = []
+    for quiz in wrapped_list:
+        match quiz.question_count:
+            case 4:
+                headers = constants.uwrs_headers_4q
+                drop_headers = constants.uwrs_drop_headers_4q
+            case 5:
+                headers = constants.uwrs_headers_5q
+                drop_headers = constants.uwrs_drop_headers_5q
+            case 6:
+                headers = constants.uwrs_headers_6q
+                drop_headers = constants.uwrs_drop_headers_6q
+            case _:
+                raise ValueError(f"Invalid number of questions: {quiz.question_count}")
 
-    def build_df_list(self, wrapped_list):
-        """
-        Build dataframe list from list of report download urls
-        :param wrapped_list: list[quiz_scraper.QuizWrapper]
-        :return: list[pandas.Dataframe]
-        """
-        df_list = []
-        for quiz in wrapped_list:
-            match quiz.question_count:
-                case 4:
-                    headers = constants.uwrs_headers_4q
-                    drop_headers = constants.uwrs_drop_headers_4q
-                case 5:
-                    headers = constants.uwrs_headers_5q
-                    drop_headers = constants.uwrs_drop_headers_5q
-                case 6:
-                    headers = constants.uwrs_headers_6q
-                    drop_headers = constants.uwrs_drop_headers_6q
-                case _:
-                    raise ValueError(f"Invalid number of questions: {quiz.question_count}")
+        df_list.append(pd.read_csv(quiz.report_download_url, header=0, names=headers)
+                       .drop(drop_headers, axis=1)
+                       )
+    return df_list
 
-            df_list.append(pd.read_csv(quiz.report_download_url, header=0, names=headers)
-                           .drop(drop_headers, axis=1)
-                           )
-        return df_list
 
-    def translate_scores(self, uwrs_df):
-        """
-        Convert the text base scores to numerical scores in the questions columns
+def translate_scores(uwrs_df):
+    """
+    Convert the text base scores to numerical scores in the questions columns
+    :param uwrs_df:
+    :return:
+    """
+    questions = []
+    scores = []
+    for i in range(1, 5):
+        score = f"score{i}"
+        scores.append(score)
+        question = f"question{i}"
+        questions.append(question)
+        uwrs_df[score] = uwrs_df[question].map(constants.answer_mapping)
+
+    return uwrs_df
+
+
+def summarize_scores(uwrs_df):
+    """
+        Return column for summary score
         :param uwrs_df:
         :return:
         """
-        questions = []
-        scores = []
-        for i in range(1, 5):
-            score = f"score{i}"
-            scores.append(score)
-            question = f"question{i}"
-            questions.append(question)
-            uwrs_df[score] = uwrs_df[question].map(constants.answer_mapping)
+    return uwrs_df[constants.scores].sum(axis=1)
 
-        return uwrs_df
 
-    @staticmethod
-    def summarize_scores(uwrs_df):
-        """
-            Return column for summary score
-            :param uwrs_df:
-            :return:
-            """
-        return uwrs_df[constants.scores].sum(axis=1)
-
-    @staticmethod
-    def normalize_sums(uwrs_df):
-        """
-        Return column for t-score conversion of summary score
-        :param uwrs_df:
-        """
-        t_score_col = uwrs_df['summary_score'].map(constants.t_score_dict)
-        # Round t score to 1 decimal since that's sig figs based on provided conversion table
-        return t_score_col.round(decimals=1)
+def normalize_sums(uwrs_df):
+    """
+    Return column for t-score conversion of summary score
+    :param uwrs_df:
+    """
+    t_score_col = uwrs_df['summary_score'].map(constants.t_score_dict)
+    # Round t score to 1 decimal since that's sig figs based on provided conversion table
+    return t_score_col.round(decimals=1)
 
 
 def create_summary_df(pre_uwrs, post_uwrs, to_csv=False, **kwargs):
@@ -146,6 +142,3 @@ def create_summary_df(pre_uwrs, post_uwrs, to_csv=False, **kwargs):
 def save_no_demographics(summary_df, term_id, prelim_ver=1):
     filename = f"[PRELIMINARY_v{prelim_ver}] {constants.valid_terms[term_id]} UWRS No Demographics.csv"
     summary_df.to_csv(f"../../reports/uwrs_out/{filename}")
-
-
-
--- src/uwrs/main.py
+++ src/uwrs/main.py
@@ -8,19 +8,18 @@ from scraper import uwrs_handler
 
 enrollment_term: int = 613
 
-canwrap = quiz_scraper.CanvasWrapper()
-qhandler = uwrs_handler.UwrsHandler(canwrap)
+# canwrap = quiz_scraper.CanvasWrapper()
 
-# pre_uwrs_wrapped = qhandler.get_uwrs_quizzes(enrollment_term, "pre")
-# post_uwrs_wrapped = qhandler.get_uwrs_quizzes(enrollment_term, "post")
+# pre_uwrs_wrapped = uwrs_handler.get_uwrs_quizzes(enrollment_term, "pre", canwrap, course_designation="HLAC")
+# post_uwrs_wrapped = uwrs_handler.get_uwrs_quizzes(enrollment_term, "post", canwrap, course_designation="HLAC")
 # with open('../../resources/pickles/pre_uwrs_wrapped.pkl', 'rb') as file:
 #     pre_uwrs_wrapped = pickle.load(file)
 # with open('../../resources/pickles/post_uwrs_wrapped.pkl', 'rb') as file:
 #     post_uwrs_wrapped = pickle.load(file)
 
 
-# pre_uwrs_df_list = qhandler.build_df_list(pre_uwrs_wrapped)
-# post_uwrs_df_list = qhandler.build_df_list(post_uwrs_wrapped)
+# pre_uwrs_df_list = uwrs_handler.build_df_list(pre_uwrs_wrapped)
+# post_uwrs_df_list = uwrs_handler.build_df_list(post_uwrs_wrapped)
 
 with open('../../resources/pickles/pre_uwrs_df_list.pkl', 'rb') as file:
     pre_uwrs_df_list = pickle.load(file)
@@ -32,13 +31,13 @@ pre_uwrs_dirty = prep_hand.concat_df(pre_uwrs_df_list)
 post_uwrs_dirty = prep_hand.concat_df(post_uwrs_df_list)
 
 pre_uwrs_no_score, post_uwrs_no_score = prep_hand.clean_dfs(pre_uwrs_dirty, post_uwrs_dirty)
-pre_uwrs = qhandler.translate_scores(pre_uwrs_no_score)
-post_uwrs = qhandler.translate_scores(post_uwrs_no_score)
+pre_uwrs = uwrs_handler.translate_scores(pre_uwrs_no_score)
+post_uwrs = uwrs_handler.translate_scores(post_uwrs_no_score)
 
 # Add columns for summary score and t-score
 for df in [pre_uwrs, post_uwrs]:
-    df['summary_score'] = qhandler.summarize_scores(df)
-    df['t_score'] = qhandler.normalize_sums(df)
+    df['summary_score'] = uwrs_handler.summarize_scores(df)
+    df['t_score'] = uwrs_handler.normalize_sums(df)
 
 # Create a summary dataframe
 # uwrs_no_demographics = uwrs_handler.create_summary_df(pre_uwrs, post_uwrs, to_csv=True, term_id=enrollment_term)
