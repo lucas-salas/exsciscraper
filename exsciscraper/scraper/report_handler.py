cf320fedbe9c7cb301032eac1525a27878b917fc
Lucas Salas <salas.lucas@gmail.com>
2023-03-13 20:27:25 -0600

    ReportHandler Methods now take single quiz
    Instead of performing operations on a list of quizzes, all the methods we refactored to perform their operation on only one quiz. This is because the multiprocessor feeds it's func argument one item of the iterable at a time, for each style.

--- exsciscraper/scraper/report_handler.py
+++ exsciscraper/scraper/report_handler.py
@@ -1,83 +1,80 @@
 import canvasapi
+import time
 
 
 class ReportHandler:
     def __init__(self, rph_canvas):
         self.rph_canvas = rph_canvas
-        self._generate_reports_has_run = False
-        self._get_progress_id_has_run = False
-        self._check_report_progress_has_run = False
 
-    def _generate_reports(self, quiz_list: list):
+    def _generate_report(self, quiz: canvasapi.quiz.Quiz):
         """Tell canvas to start generating reports for all the provided quizzes, and add the returned reports to the
         quiz object"""
-        for quiz in quiz_list:
-            try:
-                quiz.report = quiz.create_report(
-                    report_type="student_analysis", include=["file", "progress"]
-                )
-            except canvasapi.exceptions.Conflict as e:
-                print(e)
-                continue
+        try:
+            quiz.report = quiz.create_report(
+                report_type="student_analysis", include=["file", "progress"]
+            )
+        except canvasapi.exceptions.Conflict as e:
+            print(e)
+            pass
 
-            quiz.updated = False
-        self._generate_reports_has_run = True
-        return quiz_list
+        quiz.updated = False
+        return quiz
 
-    def _get_progress_id(self, quiz_list: list):
+    def _get_progress_id(self, quiz: canvasapi.quiz.Quiz):
         """Extract report progress id from progress objects"""
-        if not self._generate_reports_has_run:
-            self._generate_reports(quiz_list)
+        quiz = self._generate_report(quiz)
 
-        for quiz in quiz_list:
-            # Split url and get last element
-            quiz.report.progress_id = int(quiz.report.progress_url.split("/")[-1])
-        self._get_progress_id_has_run = True
-        return quiz_list
+        # Split url and get last element
+        quiz.report.progress_id = int(quiz.report.progress_url.split("/")[-1])
+        return quiz
 
-    def _check_report_progress(self, quiz_list: list, timeout: int = 0):
+    def _check_report_progress(self, quiz: canvasapi.quiz.Quiz, timeout: int = 1):
         """See if canvas is done generating all the reports"""
-        if not self._get_progress_id_has_run:
-            self._get_progress_id(quiz_list)
+        quiz = self._get_progress_id(quiz)
         import time
 
         # Time to be used for timeout
         time1 = time.time()
-        todo_indexes = list(range(len(quiz_list)))
+        get_progress = self.rph_canvas.get_progress
+        progress_report = get_progress(quiz.report.progress_id)
         while True:
-            # If no more indexes in todo_indexes, return true
-            if not todo_indexes:
+            if progress_report.completion == 100 and progress_report.workflow_state == "completed":
                 return True
-            for i in todo_indexes:
-                # Use canvas object to
-                progress_report = self.rph_canvas.get_progress(
-                    quiz_list[i].report.progress_id
-                )
-                if (
-                    progress_report.completion == 100
-                    and progress_report.workflow_state == "completed"
-                ):
-                    # Remove current index from todo_indexes since we no longer need to check the progress
-                    todo_indexes.remove(i)
+            progress_report = get_progress(quiz.report.progress_id)
             if timeout and time.time() - time1 > timeout:
-                print(
-                    f"_check_report_stats has timed out after {time.time() - time1} seconds."
-                )
+                print(f"_check_report_stats has timed out after {time.time() - time1} seconds.")
                 break
+            time.sleep(0.1)
+        # todo_indexes = list(range(len(quiz_list)))
+        # while True:
+        #     # If no more indexes in todo_indexes, return true
+        #     if not todo_indexes:
+        #         return True
+        #     for i in todo_indexes:
+        #         # Use canvas object to
+        #         progress_report = self.rph_canvas.get_progress(
+        #             quiz_list[i].report.progress_id
+        #         )
+        #         if (
+        #             progress_report.completion == 100
+        #             and progress_report.workflow_state == "completed"
+        #         ):
+        #             # Remove current index from todo_indexes since we no longer need to check the progress
+        #             todo_indexes.remove(i)
+
         print("Error while checking reports.")
         return False
 
-    def fetch_reports(self, quiz_list):
+    def fetch_reports(self, quiz):
         """Make sure the reports we have are updated and contain a download url"""
         print()
         # TODO add progress bar
         # TODO create variable for method
-        if self._check_report_progress(quiz_list):
-            for quiz in quiz_list:
-                tmp_report = quiz.report
-                quiz.report = quiz.get_quiz_report(tmp_report)
-                quiz.updated = True
-            return quiz_list
+        if self._check_report_progress(quiz):
+            tmp_report = quiz.report
+            quiz.report = quiz.get_quiz_report(tmp_report)
+            quiz.updated = True
+            return quiz
         else:
             print("Unable to fetch updated reports.")
             raise TimeoutError("Timed out.")
